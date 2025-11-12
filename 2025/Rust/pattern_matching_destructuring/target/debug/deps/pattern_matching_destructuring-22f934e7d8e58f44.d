ab2299c279e3fa36a61a00f237d36bd47343dd8c
devslashram <devslashram@gmail.com>
2025-11-12 18:12:11 +0800

    removing target build folder

--- 2025/Rust/pattern_matching_destructuring/target/debug/deps/pattern_matching_destructuring-22f934e7d8e58f44.d
+++
@@ -1,5 +0,0 @@
-/home/daybreak/.nb/vork_area/2025/Rust/pattern_matching_destructuring/target/debug/deps/pattern_matching_destructuring-22f934e7d8e58f44: src/main.rs
-
-/home/daybreak/.nb/vork_area/2025/Rust/pattern_matching_destructuring/target/debug/deps/pattern_matching_destructuring-22f934e7d8e58f44.d: src/main.rs
-
-src/main.rs:
--- 2025/Rust/pattern_matching_destructuring/target/debug/pattern_matching_destructuring.d
+++
@@ -1,1 +0,0 @@
-/home/daybreak/.nb/vork_area/2025/Rust/pattern_matching_destructuring/target/debug/pattern_matching_destructuring: /home/daybreak/.nb/vork_area/2025/Rust/pattern_matching_destructuring/src/main.rs
